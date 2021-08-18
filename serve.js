ade12236d1cd311e2aa857125843d5f7ee0ef825
Ruan Freitas Santos <ruan.sf0@gmail.com>
2021-08-18 17:56:20 -0300

    Servidor Heroku

---
+++ serve.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(__dirname + '/dist/frontend'));
+
+app.get('/*', (req,res) =>{
+    res.sendFile(__dirname + '/dist/frontend/');
+
+});
+
+
+app.listen(PORT, ()=>{
+    console.log('Servidor iniciado na porta'+ PORT);
+})
